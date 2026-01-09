56ae4fffea25c59e28bdbfa5f8000449cff35aa6
doni010520 <donikasumii@gmail.com>
2026-01-09 06:48:24 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,12 @@
 import pg from 'pg';
 const { Pool } = pg;
 
+// Supabase usa DATABASE_URL
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
